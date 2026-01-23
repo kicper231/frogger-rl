fbcd0eda5b4951c86f198aa09edd7d7b928e8cab
kicper231 <krol.kacper.pg2@gmail.com>
2026-01-23 08:45:19 +0100

    feat: fix visualisation

--- DQN/visualisation.py
+++ DQN/visualisation.py
@@ -1,52 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# ======================
-# WCZYTANIE DANYCH
-# ======================
+
 df = pd.read_csv(
-    "frogger_eval_extended_to_10M_rounded.csv",
-    sep=",",
-    skipinitialspace=True
+    "models_results/best_nie_normalizowane.txt", sep=",", skipinitialspace=True
 )
 
-# upewnij się, że liczby są liczbami
-for col in ["timestep", "mean_reward", "std_reward"]:
-    df[col] = pd.to_numeric(df[col], errors="coerce")
+WINDOW = 50  # im większe = gładsza linia
 
-df = df.dropna(subset=["timestep", "mean_reward", "std_reward"])
+df["reward_smooth"] = df["ep_real_reward"].rolling(window=WINDOW, min_periods=1).mean()
 
-# ======================
-# WYGŁADZANIE
-# ======================
-WINDOW = 5   # evale są rzadkie → małe okno wystarczy
-
-df["mean_reward_smooth"] = df["mean_reward"].rolling(
-    window=WINDOW, min_periods=1
-).mean()
-
-df["std_reward_smooth"] = df["std_reward"].rolling(
-    window=WINDOW, min_periods=1
-).mean()
+df["loss_smooth"] = df["loss"].rolling(window=WINDOW, min_periods=1).mean()
 
 # ======================
 # WYKRESY
 # ======================
 plt.figure(figsize=(12, 8))
 
-# ---- MEAN REWARD ----
+# ---- REWARD ----
 plt.subplot(2, 1, 1)
-plt.plot(df["timestep"], df["mean_reward_smooth"], linewidth=2)
-plt.title("Mean reward vs timestep")
-plt.ylabel("Mean reward")
+plt.plot(df["episode"], df["reward_smooth"], linewidth=2)
+plt.title("Reward per episode")
+plt.ylabel("Reward")
 plt.grid(True)
 
-# ---- EP ERROR (STD) ----
+# ---- LOSS ----
 plt.subplot(2, 1, 2)
-plt.plot(df["timestep"], df["std_reward_smooth"], linewidth=2)
-plt.title("Episode reward variability (std)")
-plt.xlabel("Timestep")
-plt.ylabel("Std reward")
+plt.plot(df["episode"], df["loss_smooth"], linewidth=2)
+plt.title("Training loss per episode")
+plt.xlabel("Episode")
+plt.ylabel("Loss")
 plt.grid(True)
 
 plt.tight_layout()
